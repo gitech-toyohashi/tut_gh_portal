378313e90f4a9438f134f5f2285fda1f996f2fe3
M47CH4N <m47ch4n@gmail.com>
2019-04-12 20:03:06 +0900

    Set up React

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,11 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
+import "../css/app.css"
 import "phoenix_html"
+import React from "react"
+import { render } from 'react-dom'
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import Home from './components/Home'
+
+render(
+  <Home />,
+  document.getElementById('app')
+)
